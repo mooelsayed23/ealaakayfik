a0875d07d3baf18c91ac28d1d123706662152d1f
ahmed <ahmed.mooelsayed@gmail.com>
2024-01-06 17:17:20 +0200

    as you like

---
+++ js/custom.js
@@ -0,0 +1,75 @@
+(function ($) {
+  "use strict";
+
+  // MENU
+  $(".navbar-collapse a").on("click", function () {
+    $(".navbar-collapse").collapse("hide");
+  });
+
+  // CUSTOM LINK
+  $(".smoothscroll").click(function () {
+    var el = $(this).attr("href");
+    var elWrapped = $(el);
+    var header_height = $(".navbar").height();
+
+    scrollToDiv(elWrapped, header_height);
+    return false;
+
+    function scrollToDiv(element, navheight) {
+      var offset = element.offset();
+      var offsetTop = offset.top;
+      var totalScroll = offsetTop - 0;
+
+      $("body,html").animate(
+        {
+          scrollTop: totalScroll,
+        },
+        300
+      );
+    }
+  });
+
+  $(".owl-carousel").owlCarousel({
+    center: true,
+    loop: true,
+    margin: 30,
+    autoplay: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 2,
+      },
+      767: {
+        items: 3,
+      },
+      1200: {
+        items: 4,
+      },
+    },
+  });
+
+  const select = (el, all = false) => {
+    el = el.trim();
+    if (all) {
+      return [...document.querySelectorAll(el)];
+    } else {
+      return document.querySelector(el);
+    }
+  };
+
+  /**
+   * Intro type effect
+   */
+  const typed = select(".typed");
+  if (typed) {
+    let typed_strings = typed.getAttribute("data-typed-items");
+    typed_strings = typed_strings.split(",");
+    new Typed(".typed", {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 130,
+      backSpeed: 60,
+      backDelay: 7000,
+    });
+  }
+})(window.jQuery);
